9c1387b5a56f585c41e6de6c5108cbe1e51aa4ec
Radhika <sahur2719@gmail.com>
2023-05-26 16:22:59 +0530

    Git done

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jbvev9wp!ak-_slh!k%is6%_n9iezon(yb(ibezj7d)^i&e6za
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,9 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL="/Images/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"Images")
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
